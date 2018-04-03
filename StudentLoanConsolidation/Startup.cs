564ebbeaeb76be20610de8a2c46faf915f9c85e0
Ihernandez11 <35441196+Ihernandez11@users.noreply.github.com>
2018-04-03 08:38:37 -0500

    Add project files.

---
+++ StudentLoanConsolidation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentLoanConsolidation.Startup))]
+namespace StudentLoanConsolidation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
